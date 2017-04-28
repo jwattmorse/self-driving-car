fe86e075346baf0e461846f68105d38ecaf4529f
Ben Solis-Cohen <bhs1@williams.edu>
2017-04-27 23:14:37 -0400

    Reading data part II: Fixed shape bug

--- read_data.py
+++ read_data.py
@@ -3,16 +3,18 @@ from numpy import genfromtxt
 from scipy import misc
 import numpy as np
 from pandas import read_csv
-
+import pandas as pd
 def read_data(folder_name):
     my_data = read_csv(folder_name + '/driving_log.csv',usecols = [0,1])    
     get_img = lambda file_name: img_from_file(folder_name,file_name.rsplit('/', 1)[-1])
-    my_data['image'] = my_data['image'].apply(get_img)
-    return (my_data['image'].as_matrix(),my_data['steering'].as_matrix())
+    images = np.ndarray(shape=(my_data.shape[0],30,32), dtype=int)
+    for i in range(my_data['image'].shape[0]):
+        images[i] = get_img(my_data['image'][0])
+    return (images,my_data['steering'].as_matrix())
 
 def img_from_file(folder_name,file_name):
     raw_img =  misc.imread(folder_name + '/IMG/' + file_name,mode = 'RGB')
-    return misc.imresize(raw_img[:,:,-1],(30,32))
+    return pd.DataFrame(misc.imresize(raw_img[:,:,-1],(30,32)))
 
 if __name__ == "__main__":
     data = read_data('data_4_27_17')
