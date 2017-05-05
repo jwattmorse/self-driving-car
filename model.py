Fixed hump to not overflow

model.py
@@ -7,7 +7,9 @@ from keras.layers import Dense # SHOULD CONFIRM
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
 from read_data import read_data as rd
+from sklearn.model_selection import train_test_split
 import numpy as np
+
 import sys
 
 max_steer = 1.0
@@ -19,10 +21,11 @@ def main ():
 def compNN():
 
     (x_train,y_train) =  rd(sys.argv[1])
-    y_train = generate_steering(y_train)
-#    y_train = np_utils.to_categorical(y_train,30)
-    x_train = x_train.astype('float32')
-    x_train /= 255
+    y = generate_steering(y_train)
+    x = x_train.astype('float32')
+    x /= 255
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
 
     # For future testing code...not currently relavent
     #x_test = x_test.reshape(x_test.shape[0], 1, 30, 32)
@@ -39,19 +42,19 @@ def compNN():
     # nb_epochs = 40 from ALVINN '88
     # verbose = 2 gives updatge after each epoch
     # shuffle set to True so that train on new samples each time
+    print(y_train.shape)
+    print(y_train[0])
+    print(y_train[0].shape)
     model.fit(x_train, y_train, batch_size = 32, epochs = 15, verbose = 2)
+
 
     # if we wanted to check how we did
     #score = model.evaluate(x_test, ytest, batchsize)
     #print score
-    print(model)
     model.save('model.h5')
 
-    #    image_array = x_train[0]
-#    image_array = np.array([[1]*960])
-
- #   image_array = np.array([x_train[0]])
-    print(model.predict(np.array([x_train[0]])))
+    # Evaluate how well the model learns the training data
+    print(model.evaluate(x_test, y_test, verbose=2))
     
     return model
 
@@ -59,16 +62,21 @@ def get_steering_angle(idx):
     total_angle = max_steer-min_steer
     frac = idx/30
     return float(frac*total_angle + min_steer)
-    
+
 def generate_steering(angles):
-    res = []
-    for angle in angles:
+    res = np.empty((len(angles),30))
+    for i in range(len(angles)):
+        angle = angles[i]
         new_y = [0]*30
         hill = [.1,.32,.61,.89,1,.89,.61,.32,.1]
         y_bin = bin(angle)
-        new_y[(y_bin-4):(y_bin+4)] = hill
-        res.append(new_y)
-    return np.array(res)
+        j = y_bin-4
+        for val in hill:
+            if j >= 0 and j <30:
+                new_y[j] = val
+            j += 1
+        res[i] = new_y
+    return res
 
 def bin(angle):
     total_angle = max_steer-min_steer