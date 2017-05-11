a161ef924b179bfe4dc63f3281cc6b08f7be2a89
Ben Solis-Cohen <bhs1@williams.edu>
2017-05-11 19:12:04 -0400

    no problems with image augmentation. About to merge in

--- read_data.py
+++ read_data.py
@@ -23,4 +23,26 @@ def img_from_file(folder_name,file_name):
 if __name__ == "__main__":
     data = read_data('data_5_7_2017_JWM2')
     print(data[1].shape)
+
+
+def read_data_test(folder_name, shape = (30,32,1), mode = 'grey', image_cols = [0]):
+    my_data = read_csv(folder_name + '/driving_log.csv',usecols = [3] + image_cols)
+    my_data.columns = ['image','steering']
+    if mode == 'colored':
+        get_img = lambda file_name: img_from_file_colored(folder_name,file_name.rsplit('/', 1)[-1],shape)
+    else:
+        get_img = lambda file_name: img_from_file_grey(folder_name,file_name.rsplit('/', 1)[-1],shape)
+    images = np.ndarray(shape = tuple([my_data.shape[0]]+list(shape)),dtype=int)
+    for i in range(my_data['image'].shape[0]):
+        images[i] = get_img(my_data['image'][0])
+    return (images,my_data['steering'].as_matrix())
+
+def img_from_file_grey(folder_name,file_name,shape = (30,32,1)):
+    raw_img =  misc.imread(folder_name + '/IMG/' + file_name,mode = 'RGB')
+    temp = misc.imresize(raw_img[:,:,-1],shape).reshape(shape)
+    return temp
+
+def img_from_file_colored(folder_name,file_name,shape = (30,32,1)):
+    raw_img =  misc.imread(folder_name + '/IMG/' + file_name,mode = 'RGB')
+    return misc.imresize(raw_img,shape)
     
